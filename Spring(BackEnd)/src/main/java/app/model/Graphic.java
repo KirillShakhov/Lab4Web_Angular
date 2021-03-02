6fdf1095f3aac1519d96f4844cf87eea2222b90b
Kirill <kirill.shakhov14@gmail.com>
2021-03-02 16:51:44 +0300

    fix back formulas

--- Spring(BackEnd)/src/main/java/app/model/Graphic.java
+++ Spring(BackEnd)/src/main/java/app/model/Graphic.java
@@ -6,9 +6,17 @@ import org.springframework.stereotype.Component;
 @Component
 public class Graphic {
     private boolean isInArea(double x, double y, double r) {
-        boolean triangle = x <= 0 && y <= 0 && y >= -(x + r)/2;
-        boolean square = x >= 0 && y <= 0 && x <= r && y >= -r/2;
-        boolean sector = x <= 0 && y >= 0 && Math.sqrt(x*x + y*y) <= r;
+        boolean triangle, square, sector;
+        if(r >= 0) {
+            triangle = x <= 0 && y <= 0 && y >= -(x + r) / 2;
+            square = x >= 0 && y <= 0 && x <= r && y >= -r / 2;
+            sector = x <= 0 && y >= 0 && Math.sqrt(x * x + y * y) <= r;
+        }
+        else{
+            triangle = x >= 0 && y >= 0 && y <= (x + Math.abs(r)) / 2;
+            square = x <= 0 && y >= 0 && x >= -Math.abs(r) && y <= Math.abs(r) / 2;
+            sector = x >= 0 && y <= 0 && Math.sqrt(x * x + y * y) <= Math.abs(r);
+        }
         return triangle || square || sector;
     }
 
