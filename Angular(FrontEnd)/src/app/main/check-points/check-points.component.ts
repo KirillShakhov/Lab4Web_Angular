a2342f2255d8776ccf91f76591e641642f462b0c
Kirill <kirill.shakhov14@gmail.com>
2021-03-06 21:01:55 +0300

    fix validate

--- Angular(FrontEnd)/src/app/main/check-points/check-points.component.ts
+++ Angular(FrontEnd)/src/app/main/check-points/check-points.component.ts
@@ -7,132 +7,114 @@ import {Point} from '../../model/point';
 import {HistoryComponent} from '../history/history.component';
 import {Graphic} from '../../model/graphic';
 
-import {MenuItem} from 'primeng/api';
-import {SelectItem} from 'primeng/api';
-import {SelectItemGroup} from 'primeng/api';
-
 
 @Component({
-  providers: [HistoryComponent],
-  selector: 'app-check-points',
-  templateUrl: './check-points.component.html',
-  styleUrls: ['./check-points.component.css'],
-  encapsulation: ViewEncapsulation.None
+    providers: [HistoryComponent],
+    selector: 'app-check-points',
+    templateUrl: './check-points.component.html',
+    styleUrls: ['./check-points.component.css'],
+    encapsulation: ViewEncapsulation.None
 })
 export class CheckPointsComponent implements OnInit {
 
-  @ViewChild('canvas', { static: true })
-  canvas: ElementRef;
+    @ViewChild('canvas', { static: true })
+    canvas: ElementRef;
 
-  point: Point = new Point(0, 0, 1, false);
-  errorMessage: string;
-  private rightX = [-2, -1.5, -1, -0.5, 0, 0.5, 1, 1.5, 2];
-  private rightR = [-2, -1.5, -1, -0.5, 0, 0.5, 1, 1.5, 2];
-  private graphic: Graphic;
+    point: Point = new Point(0, 0, 1, false);
+    errorMessage: string;
+    private rightX = ['-2', '-1.5', '-1', '-0.5', '0', '0.5', '1', '1.5', '2'];
+    private rightR = ['-2', '-1.5', '-1', '-0.5', '0', '0.5', '1', '1.5', '2'];
+    private graphic: Graphic;
 
-  selectedCountry: any;
+    constructor(private service: PointsService, private authService: AuthService) {}
 
-  countries: any[];
-
-  filteredCountries: any[];
-
-  selectedCountries: any[];
-
-  selectedCountryAdvanced: any[];
-
-  filteredBrands: any[];
-  private countryService: string[];
-
-
-  constructor(private service: PointsService, private authService: AuthService) {}
-
-  ngOnInit() {
-    this.point.x = -1;
-    this.graphic = new Graphic(this.canvas);
-    this.drawGraphic(1);
-
-    this.countryService = ['1', '2', '3', '4', '11', '11111'];
-  }
-
-  setR(value) {
-    this.point.r = value;
-  }
-
-  setX(value) {
-    this.point.x = value;
-  }
-
-  addPoint() {
-    console.log('adding point');
-
-    if (!isNumeric(this.point.y) || !(-3 < this.point.y && this.point.y < 3)) {
-      this.error('Wrong y value');
-      return false;
-    } else if (!isNumeric(this.point.x) && !(this.rightX.includes(this.point.x))) {
-      this.error('Wrong x value');
-      return false;
-    } else if (!isNumeric(this.point.r) && !(this.rightR.includes(this.point.r)) || !(-2 < this.point.r && this.point.r < 2)) {
-      this.error('Wrong r value');
-      return false;
+    ngOnInit() {
+        this.point.x = -1;
+        this.graphic = new Graphic(this.canvas);
+        this.drawGraphic(1);
     }
 
-    this.service.addPoint(this.point).then(data => {
-      this.drawPoint(<Point>data);
-      this.service.getPoints();
-    }).catch((err: HttpErrorResponse) => {
-      console.log('err');
-      if (err.status === 401 || err.status === 403) {
-        this.authService.logOut();
-      }
-    });
-    return true;
-  }
+    setR(value) {
+        this.point.r = value;
+    }
 
-  getPointsRecalculated(r) {
-    console.log('getting points');
-    this.service.getPointsRecalculated(r).subscribe(data => (data as Point[]).forEach(p => this.drawPoint(p)), (err: HttpErrorResponse) => {
-      console.log('err');
-      if (err.status === 401 || err.status === 403 ) {
-        this.authService.logOut();
-      }
-    });
-  }
+    setX(value) {
+        this.point.x = value;
+    }
 
-  addPointFromCanvas() {
-    console.log('Click on canvas');
+    addPoint() {
+        console.log('adding point');
+        // Валидация на фронте
+        if (!isNumeric(this.point.y) || !(-3 < this.point.y && this.point.y < 3)) {
+            this.error('Wrong y value');
+            return false;
+        } else if (!isNumeric(this.point.x) || !(-2 <= this.point.x && this.point.x <= 2)) {
+            this.error('Wrong x value');
+            return false;
+        } else if (!isNumeric(this.point.r) || !(this.rightR.includes(String(this.point.r)))
+            || !(-2 <= this.point.r && this.point.r <= 2)) {
+            this.error('Wrong r value');
+            return false;
+        }
 
-    const br = this.canvas.nativeElement.getBoundingClientRect();
-    const left = br.left;
-    const top = br.top;
+        this.service.addPoint(this.point).then(data => {
+            this.drawPoint(<Point>data);
+            this.service.getPoints();
+        }).catch((err: HttpErrorResponse) => {
+            console.log('err');
+            if (err.status === 401 || err.status === 403) {
+                this.authService.logOut();
+            }
+        });
+        return true;
+    }
 
-    const event: MouseEvent = <MouseEvent> window.event;
-    const x = event.clientX - left - 5;
-    const y = event.clientY - top - 5;
+    getPointsRecalculated(r) {
+        console.log('getting points');
+        this.service.getPointsRecalculated(r).subscribe(data => (data as Point[]).forEach(p => this.drawPoint(p)),
+             (err: HttpErrorResponse) => {
+            console.log('err');
+            if (err.status === 401 || err.status === 403 ) {
+                this.authService.logOut();
+            }
+        });
+    }
 
-    const xCalculated = (x - 150) / 130 * 5;
-    const yCalculated = (-y + 150) / 130 * 5;
+    addPointFromCanvas() {
+        console.log('Click on canvas');
 
-    this.point.x = xCalculated;
-    this.point.y = yCalculated;
+        const br = this.canvas.nativeElement.getBoundingClientRect();
+        const left = br.left;
+        const top = br.top;
 
-    this.addPoint();
-  }
+        const event: MouseEvent = <MouseEvent> window.event;
+        const x = event.clientX - left - 5;
+        const y = event.clientY - top - 5;
 
-  drawPoint(point: Point) {
-    this.graphic.drawPoint(point);
-  }
+        const xCalculated = (x - 150) / 130 * 5;
+        const yCalculated = (-y + 150) / 130 * 5;
 
-  drawGraphic(r) {
-    this.graphic.drawGraphic(r);
-    this.getPointsRecalculated(r);
-  }
+        this.point.x = xCalculated;
+        this.point.y = yCalculated;
 
-  isDesktopDisplay() {
-    return document.body.clientWidth >= 672;
-  }
+        this.addPoint();
+    }
 
-  private error(message: string) {
-    this.errorMessage = message;
-    setTimeout(() => {this.errorMessage = null; }, 3000);
-  }
+    drawPoint(point: Point) {
+        this.graphic.drawPoint(point);
+    }
+
+    drawGraphic(r) {
+        this.graphic.drawGraphic(r);
+        this.getPointsRecalculated(r);
+    }
+
+    isDesktopDisplay() {
+        return document.body.clientWidth >= 672;
+    }
+
+    private error(message: string) {
+        this.errorMessage = message;
+        setTimeout(() => {this.errorMessage = null; }, 3000);
+    }
 }
